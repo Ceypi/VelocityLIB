b445ffa15bf7ee38fe1c5df0760e96bcccc311da
Ceypi <44949167+Ceypi@users.noreply.github.com>
2024-05-06 00:52:46 +0200

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,4 +1,4 @@
-local library = loadstring(game:HttpGet("https://github.com/GhostDuckyy/UI-Libraries/blob/main/Velocity.vip/source.lua?raw=true"))() do --// ui main
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ceypi/VelocityLIB/ca5d6025e71e4895ec942ec68b8719a0afd88ecc/source.lua"))() do --// ui main
     local ui = {} do
         ui.window = library.init_window("Window", {size = Vector2.new(600, 420), name = "Concert", drag_tween = true})
         ui.pages = {} do
