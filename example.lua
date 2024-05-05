83ed5c3dc5bf9247feccf35aa0991ead7e6009d3
Ceypi <44949167+Ceypi@users.noreply.github.com>
2024-05-06 01:07:35 +0200

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,4 +1,4 @@
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ceypi/VelocityLIB/a26376e3ef033377c784b96f58cec95d96b7f058/AntiSynXSource.lua"))() do --// ui main
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ceypi/VelocityLIB/b66a5dea7a56748ddd8bdcf812d1d905b43c51c4/AntiSynXSource.lua"))() do --// ui main
     local ui = {} do
         ui.window = library.init_window("Window", {size = Vector2.new(600, 420), name = "Concert", drag_tween = true})
         ui.pages = {} do
